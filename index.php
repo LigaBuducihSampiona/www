80a3889a86c6c39439d48ee3b1d33576d3c3ec57
Voja <voislavj@gmail.com>
2015-11-17 23:42:15 +0100

    youtube video optimization; css mobile optimizatin

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
-<?php
+﻿<?php
 
 	$title = "Home";
 
@@ -18,26 +18,37 @@
 				<section>					
 					<!--  ***************slider*************-->
 					<h3>Najnovije vesti</h3>
-					<div id="sliderFrame">
+					<!--<div id="sliderFrame">
 						<div id="slider">
 							<a href="http://www.menucool.com/javascript-image-slider" target="_blank">
-								<img src="plugins/imgslider/images/image-slider-1.jpg" alt="Welcome to Menucool.com" />
+								<img src="plugins/imgslider/images/image-slider-1.jpg" alt="" />
 							</a>
-							<img src="plugins/imgslider/images/image-slider-2.jpg" alt="Proizvoljan opis" />
-							<img src="plugins/imgslider/images/image-slider-3.jpg" alt="Proizvoljan opis." />
-							<img src="plugins/imgslider/images/image-slider-4.jpg" alt="#htmlcaption" />
+							<img src="plugins/imgslider/images/image-slider-2.jpg" alt="" />
+							<img src="plugins/imgslider/images/image-slider-3.jpg" alt="" />
+							<img src="plugins/imgslider/images/image-slider-4.jpg" alt="" />
 							<img src="plugins/imgslider/images/image-slider-5.jpg" />
 						</div>
 						<div id="htmlcaption" style="display: none;">
 							<em>HTML</em> caption. Link to <a href="http://www.google.com/">Google</a>.
 						</div>
 						<br/><br/>
-					</div>
+					</div>-->
 					<p>
-						Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam faucibus bibendum odio, quis convallis erat faucibus vitae. Morbi iaculis sagittis libero, eu ultrices urna finibus sed. Aenean tincidunt in lectus id faucibus. Nulla facilisi. Proin at dui ultricies, molestie quam a, viverra dui. Pellentesque blandit diam sed turpis imperdiet, a imperdiet ante ultricies. Donec at quam erat. Sed varius, ipsum vitae porttitor gravida, turpis lectus vehicula urna, eu commodo ante erat vel ligula. Mauris commodo pellentesque lorem, in tristique tellus ornare quis. Nam eu tempor ligula. Integer tortor orci, fermentum et euismod non, bibendum et enim. Aliquam lobortis efficitur sem eget egestas. 
+						Pregled prvog kola
 					</p>
+					 <iframe style="padding-left:15px;" class="youtube-video" src="https://www.youtube.com/embed/tCjcz5KJnpI" frameborder="0" allowfullscreen></iframe>
+					 
+					 <p>
+						<br>
+						Odigrano je 1. kolo lige budućih šampiona.<br/>
+						Na stranici <a href="rezultati.php">rezultata</a> možete pogledati tabele i rezultate.
+					</p>
+					 <iframe style="padding-left:15px;" class="youtube-video" src="https://www.youtube.com/embed/MoDWueAq7dU" frameborder="0" allowfullscreen></iframe>
+					 
+					 
+					
 				</section>
-				<aside><? include "template/tabela.php" ?></aside>
+				<!--<aside><? include "template/tabela.php" ?></aside>-->
 			</div>
 			
 			<?php include "template/footer.php" ?>
